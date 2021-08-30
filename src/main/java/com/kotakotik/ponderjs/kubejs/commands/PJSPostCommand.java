5e7fff7aa062b12a53bc10e63addd38162e8dade
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-08-30 15:52:27 +0300

    aaaaaaaa more info on command thing aaaa

--- src/main/java/com/kotakotik/ponderjs/kubejs/commands/PJSPostCommand.java
+++ src/main/java/com/kotakotik/ponderjs/kubejs/commands/PJSPostCommand.java
@@ -11,8 +11,8 @@ public class PJSPostCommand implements Command<CommandSource> {
     public static String r(boolean requireReload) {
         try {
             PonderRegistryEventJS.runAllRegistration();
-            return "Reran all ponder registration successfully" + (requireReload ? ", press F3+T to reload scenes" : "") + " \n"+
-                                    "Note: This reload is pretty janky with tags so I recommend only using it to reload scenes";
+            return "Reran all ponder registration successfully" + (requireReload ? ", press F3+T to reload scenes" : "") + " \n" +
+                    "Note: This reload is pretty janky with tags and it can't remove scenes so I recommend only using it to reload scenes";
         } catch (Exception e) {
             e.printStackTrace();
             return "err";
