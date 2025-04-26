140934afcae20a8da8aa18f2fc7870268547e120
LLytho <main@lytho.dev>
2025-04-26 15:35:08 +0200

    allow better creation of tags

--- run/kubejs/client_scripts/tags.js
+++ run/kubejs/client_scripts/tags.js
@@ -4,6 +4,21 @@ Ponder.tags((event) => {
         "minecraft:shears",
     ]);
 
+    event.createTag("ponderjs:with_index_no_add", tag => {
+        tag.icon("minecraft:diamond");
+        tag.title("Add to index, but dont add icon");
+        tag.description("Diamond Description");
+        tag.noIndex();
+    });
+
+    event.createTag("ponderjs:with_index_with_add", tag => {
+        tag.icon("minecraft:emerald");
+        tag.title("Add to index, but dont add icon");
+        tag.description("Emerald Description");
+        // tag.noIndex();
+        tag.addIconToItems();
+    });
+
     try {
         event.add("create:kinetic_appliances", "minecraft:dirt");
         event.remove("create:kinetic_appliances", ["create:deployer", "create:encased_fan"]);
--- src/main/java/com/almostreliable/ponderjs/PonderItemTagEventJS.java
+++ src/main/java/com/almostreliable/ponderjs/PonderItemTagEventJS.java
@@ -1,15 +1,16 @@
 package com.almostreliable.ponderjs;
 
 import com.almostreliable.ponderjs.util.PonderPlatform;
+import com.google.common.base.Preconditions;
 import dev.latvian.mods.kubejs.event.EventJS;
 import dev.latvian.mods.kubejs.util.ConsoleJS;
 import net.createmod.ponder.api.registration.PonderTagRegistrationHelper;
-import net.createmod.ponder.api.registration.TagBuilder;
 import net.createmod.ponder.foundation.PonderTag;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import javax.annotation.Nullable;
@@ -31,14 +32,9 @@ public class PonderItemTagEventJS extends EventJS {
         var idWithNameSpace = PonderJS.appendKubeToId(id);
         PonderJS.NAMESPACES.add(idWithNameSpace.getNamespace());
 
-        var tagBuilder = helper.registerTag(idWithNameSpace);
-        var builder = new Builder(tagBuilder);
+        var builder = new Builder(idWithNameSpace);
         onCreate.accept(builder);
-        tagBuilder.register();
-
-        for (var itemId : builder.getItemIds()) {
-            helper.addTagToComponent(itemId, idWithNameSpace);
-        }
+        builder.fin(helper);
     }
 
     public void createTag(String id, Item displayItem, String title, String description, @Nullable Ingredient ingredient) {
@@ -46,7 +42,6 @@ public class PonderItemTagEventJS extends EventJS {
             builder.icon(displayItem);
             builder.title(title);
             builder.description(description);
-            builder.addToIndex();
             if (ingredient != null) builder.items(ingredient);
         });
     }
@@ -100,40 +95,42 @@ public class PonderItemTagEventJS extends EventJS {
     @SuppressWarnings("UnusedReturnValue")
     public static class Builder {
 
-        private final TagBuilder tagBuilder;
         private final LinkedHashSet<ResourceLocation> itemIds = new LinkedHashSet<>();
+        private final ResourceLocation id;
+        @Nullable
+        private String title;
+        @Nullable
+        private String description;
+        private Item itemIcon = Items.BARRIER;
+        private boolean noIndex = false;
+        private boolean addItemIconToItems = false;
 
-        private Builder(TagBuilder tagBuilder) {
-            this.tagBuilder = tagBuilder;
+        private Builder(ResourceLocation id) {
+            this.id = id;
         }
 
         public Builder title(String title) {
-            tagBuilder.title(title);
+            this.title = title;
             return this;
         }
 
         public Builder description(String description) {
-            tagBuilder.description(description);
-            return this;
-        }
-
-        public Builder addToIndex() {
-            tagBuilder.addToIndex();
+            this.description = description;
             return this;
         }
 
         public Builder icon(Item item) {
-            tagBuilder.item(item);
+            this.itemIcon = item;
             return this;
         }
 
-        public Builder icon(Item item, boolean addAsItem) {
-            tagBuilder.item(item, true, addAsItem);
+        public Builder noIndex() {
+            this.noIndex = true;
             return this;
         }
 
-        public Builder iconTexture(String path) {
-            tagBuilder.icon(path).idAsIcon();
+        public Builder addIconToItems() {
+            addItemIconToItems = true;
             return this;
         }
 
@@ -146,8 +143,19 @@ public class PonderItemTagEventJS extends EventJS {
             return this;
         }
 
-        private Set<ResourceLocation> getItemIds() {
-            return itemIds;
+        private void fin(PonderTagRegistrationHelper<ResourceLocation> helper) {
+            Preconditions.checkNotNull(title, "Title cannot be null for tag " + id);
+            Preconditions.checkNotNull(description, "Description cannot be null for tag " + id);
+
+            var tagBuilder = helper.registerTag(id).title(title).description(description);
+            if (!noIndex) tagBuilder.addToIndex();
+            tagBuilder.item(itemIcon, true, addItemIconToItems);
+
+            tagBuilder.register();
+
+            for (var itemId : itemIds) {
+                helper.addTagToComponent(itemId, id);
+            }
         }
     }
 }
